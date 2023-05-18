4cfb42edbbf1c014d2fd1d15b0541cdf68c28187
xiaoling liang <liangxiaoling@phoenixtv.com>
2023-05-18 15:55:01 +0800

    feat: 按需引入style

--- plugins/quasar.ts
+++ plugins/quasar.ts
@@ -3,8 +3,21 @@ import { Plugin } from "@nuxt/types";
 import Quasar from 'quasar/src/vue-plugin'
 // 引入基础sass
 import '@quasar/extras/material-icons/material-icons.css'
-import 'quasar/dist/quasar.min.css'
-
+import 'quasar/src/css/core/animations.sass'
+import 'quasar/src/css/core/colors.sass'
+import 'quasar/src/css/core/elevation.sass'
+import 'quasar/src/css/core/flex.sass'
+import 'quasar/src/css/core/helpers.sass'
+import 'quasar/src/css/core/mouse.sass'
+import 'quasar/src/css/core/orientation.sass'
+import 'quasar/src/css/core/positioning.sass'
+import 'quasar/src/css/core/size.sass'
+import 'quasar/src/css/core/touch.sass'
+import 'quasar/src/css/core/transitions.sass'
+import 'quasar/src/css/core/typography.sass'
+import 'quasar/src/css/core/visibility.sass'
+import 'quasar/src/css/core/dark.sass'
+import 'quasar/src/css/normalize.sass'
 
 const quasarPlugin: Plugin = ({app, ssrContext}) => {
   Vue.use(Quasar)
--- webpack/QuasarResolver.ts
+++ webpack/QuasarResolver.ts
@@ -1,17 +1,33 @@
+import { promises as fs } from 'node:fs'
 import { resolveModule } from 'local-pkg'
 import { ComponentResolver } from 'unplugin-vue-components'
-export default function QuasarResolver(): ComponentResolver {
-  const getQuasarFunPath = require('quasar/dist/babel-transforms/imports.js')
+const getQuasarFullPath = require('quasar/dist/babel-transforms/imports.js')
+async function getSideEffects(componentName: string, sideEffects: string[], isLoaded: string[]) {
+  if (isLoaded.includes(componentName)) return
+  isLoaded.push(componentName)
+
+  const path = getQuasarFullPath(componentName)
+  const localPath = resolveModule(path) as string
+  const cssPath = path.replace('.js', '.sass')
+  const cssLocalPath = resolveModule(cssPath) || ''
+  if (cssLocalPath && !sideEffects.includes(cssPath)) sideEffects.push(cssPath)
+  if (!localPath) return
+  const content = await fs.readFile(localPath, 'utf-8')
+  const referencedComponents = content.match(/Q\w*(?=\.js)/g)
+  if (referencedComponents) {
+    await Promise.allSettled(referencedComponents.map(item => getSideEffects(item, sideEffects, isLoaded)))
+  }
+}
+export default  function QuasarResolver(): ComponentResolver {
   return {
     type: 'component',
-    resolve: (name: string) => {
+    resolve: async (name: string) => {
       try {
-        const path = getQuasarFunPath(name)
-        const cssPath = path.replace('.js', '.sass')
-        const cssLocalPath = resolveModule(cssPath) || ''
-        return { as: name, from: path, sideEffects: cssLocalPath ? [
-          cssPath
-        ] : []}
+        const isLoaded: string[] = []
+        const path = getQuasarFullPath(name)
+        const sideEffects: string[] = []
+        await getSideEffects(name, sideEffects, isLoaded)
+        return { as: name, from: path, sideEffects}
       } catch (error) {
       }
     }
