00e6f02ffa7efc8607c3a00a37e612ff81eb840a
xiaoling liang <liangxiaoling@phoenixtv.com>
2023-05-18 11:35:05 +0800

    styles: on-demand import of quasar styles

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is a practical project using Nuxt2.0 and incorporating Quasar1.0. The following tasks have been completed:
 
 - [x] On-demand import of Quasar components
-- [] On-demand import of Quasar styles (On-demand imports have been done, but many styles for components are not included in the component’s SASS)
+- [x] On-demand import of Quasar styles
 - [x] Support for SSR (Server-Side Rendering), as well as support for related “$q” functionality
 - [x] Support for mobile, class
 - [x] Support for SASS.
--- plugins/quasar.ts
+++ plugins/quasar.ts
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import { Plugin } from "@nuxt/types";
 import Quasar from 'quasar/src/vue-plugin'
-import 'quasar/dist/quasar.min.css'
+// 引入基础sass
+import 'quasar/src/css/core/animations.sass'
+import 'quasar/src/css/core/colors.sass'
+import 'quasar/src/css/core/elevation.sass'
+import 'quasar/src/css/core/flex.sass'
+import 'quasar/src/css/core/helpers.sass'
+import 'quasar/src/css/core/mouse.sass'
+import 'quasar/src/css/core/orientation.sass'
+import 'quasar/src/css/core/positioning.sass'
+import 'quasar/src/css/core/size.sass'
+import 'quasar/src/css/core/touch.sass'
+import 'quasar/src/css/core/transitions.sass'
+import 'quasar/src/css/core/typography.sass'
+import 'quasar/src/css/core/visibility.sass'
+import 'quasar/src/css/core/dark.sass'
+import 'quasar/src/css/normalize.sass'
+
 const quasarPlugin: Plugin = ({app, ssrContext}) => {
   Vue.use(Quasar)
   Quasar.ssrUpdate({app, ssr: ssrContext})
